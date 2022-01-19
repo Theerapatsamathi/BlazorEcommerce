3e1df1703bbf88d5cf57de9a9924740384e67f05
Theerapatsamathi <s6235l10001@sau.ac.th>
2022-01-19 11:28:18 +0700

    Add Variants

--- BlazorMCIC/Server/Migrations/20220119034230_ProductVariants.Designer.cs
+++ BlazorMCIC/Server/Migrations/20220119034230_ProductVariants.Designer.cs
@@ -10,8 +10,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlazorMCIC.Server.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20220118082723_EditionSeeding")]
-    partial class EditionSeeding
+    [Migration("20220119034230_ProductVariants")]
+    partial class ProductVariants
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
@@ -83,31 +83,36 @@ namespace BlazorMCIC.Server.Migrations
                         new
                         {
                             Id = 1,
-                            Name = "Paperback"
+                            Name = "Default"
                         },
                         new
                         {
                             Id = 2,
-                            Name = "E-Book"
+                            Name = "Paperback"
                         },
                         new
                         {
                             Id = 3,
-                            Name = "Audiobook"
+                            Name = "E-Book"
                         },
                         new
                         {
                             Id = 4,
-                            Name = "PC"
+                            Name = "Audiobook"
                         },
                         new
                         {
                             Id = 5,
-                            Name = "PlayStation"
+                            Name = "PC"
                         },
                         new
                         {
                             Id = 6,
+                            Name = "PlayStation"
+                        },
+                        new
+                        {
+                            Id = 7,
                             Name = "Xbox"
                         });
                 });
@@ -140,12 +145,6 @@ namespace BlazorMCIC.Server.Migrations
                     b.Property<bool>("IsPublic")
                         .HasColumnType("bit");
 
-                    b.Property<decimal>("OriginalPrice")
-                        .HasColumnType("decimal(18,2)");
-
-                    b.Property<decimal>("Price")
-                        .HasColumnType("decimal(18,2)");
-
                     b.Property<string>("Title")
                         .HasColumnType("nvarchar(max)");
 
@@ -161,161 +160,213 @@ namespace BlazorMCIC.Server.Migrations
                             Id = 1,
                             CategoryId = 1,
                             DateCreated = new DateTime(2021, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
-                            Description = "IIIIIIIIIIIIIIIIIII",
+                            Description = "The Hitchhiker's Guide to the Galaxy (sometimes referred to as HG2G, HHGTTG, H2G2, or tHGttG) is a comedy science fiction series created by Douglas Adams.",
                             Image = "https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg",
                             IsDeleted = false,
                             IsPublic = false,
-                            OriginalPrice = 19.99m,
-                            Price = 9.99m,
-                            Title = "Thee Gitchi"
+                            Title = "The Hitchhiker's Guide to the Galaxy"
                         },
                         new
                         {
                             Id = 2,
-                            CategoryId = 2,
+                            CategoryId = 1,
                             DateCreated = new DateTime(2021, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
-                            Description = "IIIIIIIIIIIIIIIIIII",
-                            Image = "https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg",
+                            Description = "Ready Player One is a 2011 science fiction novel, and the debut novel of American author Ernest Cline. The story, set in a dystopia in 2045, follows protagonist Wade Watts on his search for an Easter egg in a worldwide virtual reality game, the discovery of which would lead him to inherit the game creator's fortune.",
+                            Image = "https://upload.wikimedia.org/wikipedia/en/a/a4/Ready_Player_One_cover.jpg",
                             IsDeleted = false,
                             IsPublic = false,
-                            OriginalPrice = 19.99m,
-                            Price = 9.99m,
-                            Title = "Thee IPSSSSS"
+                            Title = "Ready Player One"
                         },
                         new
                         {
                             Id = 3,
                             CategoryId = 1,
                             DateCreated = new DateTime(2021, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
-                            Description = "IIIIIIIIIIIIIIIIIII",
-                            Image = "https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg",
+                            Description = "Nineteen Eighty-Four: A Novel, often published as 1984, is a dystopian social science fiction novel by English novelist George Orwell. It was published on 8 June 1949 by Secker & Warburg as Orwell's ninth and final book completed in his lifetime.",
+                            Image = "https://upload.wikimedia.org/wikipedia/commons/c/c3/1984first.jpg",
                             IsDeleted = false,
                             IsPublic = false,
-                            OriginalPrice = 19.99m,
-                            Price = 9.99m,
-                            Title = "Thee Gitchi222"
+                            Title = "Nineteen Eighty-Four"
                         },
                         new
                         {
                             Id = 4,
                             CategoryId = 2,
                             DateCreated = new DateTime(2021, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
-                            Description = "IIIIIIIIIIIIIIIIIII",
-                            Image = "https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg",
+                            Description = "The Pentax Spotmatic refers to a family of 35mm single-lens reflex cameras manufactured by the Asahi Optical Co. Ltd., later known as Pentax Corporation, between 1964 and 1976.",
+                            Image = "https://upload.wikimedia.org/wikipedia/commons/e/e9/Honeywell-Pentax-Spotmatic.jpg",
                             IsDeleted = false,
                             IsPublic = false,
-                            OriginalPrice = 19.99m,
-                            Price = 9.99m,
-                            Title = "Thee IPSSSSS33"
+                            Title = "Pentax Spotmatic"
                         },
                         new
                         {
                             Id = 5,
-                            CategoryId = 1,
+                            CategoryId = 2,
                             DateCreated = new DateTime(2021, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
-                            Description = "IIIIIIIIIIIIIIIIIII",
-                            Image = "https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg",
+                            Description = "The Xbox is a home video game console and the first installment in the Xbox series of video game consoles manufactured by Microsoft.",
+                            Image = "https://upload.wikimedia.org/wikipedia/commons/4/43/Xbox-console.jpg",
                             IsDeleted = false,
                             IsPublic = false,
-                            OriginalPrice = 19.99m,
-                            Price = 9.99m,
-                            Title = "Thee Gitchi444"
+                            Title = "Xbox"
                         },
                         new
                         {
                             Id = 6,
                             CategoryId = 2,
                             DateCreated = new DateTime(2021, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
-                            Description = "IIIIIIIIIIIIIIIIIII",
-                            Image = "https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg",
+                            Description = "The Super Nintendo Entertainment System (SNES), also known as the Super NES or Super Nintendo, is a 16-bit home video game console developed by Nintendo that was released in 1990 in Japan and South Korea.",
+                            Image = "https://upload.wikimedia.org/wikipedia/commons/e/ee/Nintendo-Super-Famicom-Set-FL.jpg",
                             IsDeleted = false,
                             IsPublic = false,
-                            OriginalPrice = 19.99m,
-                            Price = 9.99m,
-                            Title = "Thee IPSSSSS"
+                            Title = "Super Nintendo Entertainment System"
                         },
                         new
                         {
                             Id = 7,
-                            CategoryId = 1,
+                            CategoryId = 3,
                             DateCreated = new DateTime(2021, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
-                            Description = "IIIIIIIIIIIIIIIIIII",
-                            Image = "https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg",
+                            Description = "Half-Life 2 is a 2004 first-person shooter game developed and published by Valve. Like the original Half-Life, it combines shooting, puzzles, and storytelling, and adds features such as vehicles and physics-based gameplay.",
+                            Image = "https://upload.wikimedia.org/wikipedia/en/2/25/Half-Life_2_cover.jpg",
                             IsDeleted = false,
                             IsPublic = false,
-                            OriginalPrice = 19.99m,
-                            Price = 9.99m,
-                            Title = "Thee Gitchi"
+                            Title = "Half-Life 2"
                         },
                         new
                         {
                             Id = 8,
                             CategoryId = 3,
                             DateCreated = new DateTime(2021, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
-                            Description = "IIIIIIIIIIIIIIIIIII",
-                            Image = "https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg",
+                            Description = "Diablo II is an action role-playing hack-and-slash computer video game developed by Blizzard North and published by Blizzard Entertainment in 2000 for Microsoft Windows, Classic Mac OS, and macOS.",
+                            Image = "https://upload.wikimedia.org/wikipedia/en/d/d5/Diablo_II_Coverart.png",
                             IsDeleted = false,
                             IsPublic = false,
-                            OriginalPrice = 19.99m,
-                            Price = 9.99m,
-                            Title = "Thee IPSSSSS"
+                            Title = "Diablo II"
+                        },
+                        new
+                        {
+                            Id = 9,
+                            CategoryId = 3,
+                            DateCreated = new DateTime(2021, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                            Description = "Day of the Tentacle, also known as Maniac Mansion II: Day of the Tentacle, is a 1993 graphic adventure game developed and published by LucasArts. It is the sequel to the 1987 game Maniac Mansion.",
+                            Image = "https://upload.wikimedia.org/wikipedia/en/7/79/Day_of_the_Tentacle_artwork.jpg",
+                            IsDeleted = false,
+                            IsPublic = false,
+                            Title = "Day of the Tentacle"
                         });
                 });
 
-            modelBuilder.Entity("EditionProduct", b =>
+            modelBuilder.Entity("BlazorMCIC.Shared.ProductVariant", b =>
                 {
-                    b.Property<int>("EditionsId")
+                    b.Property<int>("ProductId")
                         .HasColumnType("int");
 
-                    b.Property<int>("ProductsId")
+                    b.Property<int>("EditionId")
                         .HasColumnType("int");
 
-                    b.HasKey("EditionsId", "ProductsId");
+                    b.Property<decimal>("OriginalPrice")
+                        .HasColumnType("decimal(18,2)");
 
-                    b.HasIndex("ProductsId");
+                    b.Property<decimal>("Price")
+                        .HasColumnType("decimal(18,2)");
 
-                    b.ToTable("EditionProduct");
+                    b.HasKey("ProductId", "EditionId");
+
+                    b.HasIndex("EditionId");
+
+                    b.ToTable("ProductVariant");
 
                     b.HasData(
                         new
                         {
-                            EditionsId = 1,
-                            ProductsId = 1
+                            ProductId = 1,
+                            EditionId = 2,
+                            OriginalPrice = 19.99m,
+                            Price = 9.99m
                         },
                         new
                         {
-                            EditionsId = 2,
-                            ProductsId = 1
+                            ProductId = 1,
+                            EditionId = 3,
+                            OriginalPrice = 0m,
+                            Price = 7.99m
                         },
                         new
                         {
-                            EditionsId = 3,
-                            ProductsId = 1
+                            ProductId = 1,
+                            EditionId = 4,
+                            OriginalPrice = 29.99m,
+                            Price = 19.99m
                         },
                         new
                         {
-                            EditionsId = 1,
-                            ProductsId = 2
+                            ProductId = 2,
+                            EditionId = 2,
+                            OriginalPrice = 14.99m,
+                            Price = 7.99m
                         },
                         new
                         {
-                            EditionsId = 2,
-                            ProductsId = 2
+                            ProductId = 3,
+                            EditionId = 2,
+                            OriginalPrice = 0m,
+                            Price = 6.99m
                         },
                         new
                         {
-                            EditionsId = 4,
-                            ProductsId = 7
+                            ProductId = 4,
+                            EditionId = 1,
+                            OriginalPrice = 249.00m,
+                            Price = 166.66m
                         },
                         new
                         {
-                            EditionsId = 5,
-                            ProductsId = 7
+                            ProductId = 5,
+                            EditionId = 1,
+                            OriginalPrice = 299m,
+                            Price = 159.99m
                         },
                         new
                         {
-                            EditionsId = 6,
-                            ProductsId = 7
+                            ProductId = 6,
+                            EditionId = 1,
+                            OriginalPrice = 400m,
+                            Price = 73.74m
+                        },
+                        new
+                        {
+                            ProductId = 7,
+                            EditionId = 5,
+                            OriginalPrice = 29.99m,
+                            Price = 19.99m
+                        },
+                        new
+                        {
+                            ProductId = 7,
+                            EditionId = 6,
+                            OriginalPrice = 0m,
+                            Price = 69.99m
+                        },
+                        new
+                        {
+                            ProductId = 7,
+                            EditionId = 7,
+                            OriginalPrice = 59.99m,
+                            Price = 49.99m
+                        },
+                        new
+                        {
+                            ProductId = 8,
+                            EditionId = 5,
+                            OriginalPrice = 24.99m,
+                            Price = 9.99m
+                        },
+                        new
+                        {
+                            ProductId = 9,
+                            EditionId = 5,
+                            OriginalPrice = 0m,
+                            Price = 14.99m
                         });
                 });
 
@@ -330,19 +381,28 @@ namespace BlazorMCIC.Server.Migrations
                     b.Navigation("Category");
                 });
 
-            modelBuilder.Entity("EditionProduct", b =>
+            modelBuilder.Entity("BlazorMCIC.Shared.ProductVariant", b =>
                 {
-                    b.HasOne("BlazorMCIC.Shared.Edition", null)
+                    b.HasOne("BlazorMCIC.Shared.Edition", "Edition")
                         .WithMany()
-                        .HasForeignKey("EditionsId")
+                        .HasForeignKey("EditionId")
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
 
-                    b.HasOne("BlazorMCIC.Shared.Product", null)
-                        .WithMany()
-                        .HasForeignKey("ProductsId")
+                    b.HasOne("BlazorMCIC.Shared.Product", "Product")
+                        .WithMany("Variants")
+                        .HasForeignKey("ProductId")
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
+
+                    b.Navigation("Edition");
+
+                    b.Navigation("Product");
+                });
+
+            modelBuilder.Entity("BlazorMCIC.Shared.Product", b =>
+                {
+                    b.Navigation("Variants");
                 });
 #pragma warning restore 612, 618
         }
--- BlazorMCIC/Shared/Edition.cs
+++ BlazorMCIC/Shared/Edition.cs
@@ -11,8 +11,7 @@ namespace BlazorMCIC.Shared
     {
         public int Id { get; set; } 
         public string Name { get; set; }   
-        [JsonIgnore]
-        public List<Product> Products { get; set; }
+
 
     }
 }
